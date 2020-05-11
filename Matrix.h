8d358fba58af63f3d34ab8800105740e284909fa
wmharri <wharr28@lsu.edu>
2020-05-11 15:34:43 -0500

    Fixed Delta(int) function

--- Matrix.h
+++ Matrix.h
@@ -1,10 +1,3 @@
-/*
- * Matrix.h
- *
- *  Created on: Dec 16, 2018
- *      Author: Michael Harrison
- */
-
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
@@ -12,12 +5,16 @@ class Matrix {
 public:
 	Matrix(const int row, const int column);
 	~Matrix();
+	Matrix(const Matrix &obj);
+	Matrix& operator=(Matrix other);
 	Matrix& operator+(const Matrix& summand);
 	Matrix& operator*(const Matrix& multiplier);
 	void setEntry(int rowIndex, int colIndex, int entry);
 	int getRow() const;
 	int getColumn() const;
 	int** getMatrix() const;
+
+	static Matrix Delta(/*Matrix matrix, */int size);
 
 private:
 	int rowSize;
