7f9719967212ea010c5bb34fe0ec4f9743f913f8
wmharri <wharr28@lsu.edu>
2020-05-11 15:33:48 -0500

    Fixed Delta(int) function

--- Matrix.cpp
+++ Matrix.cpp
@@ -11,6 +11,8 @@
 // class constructor for Matrix
 Matrix::Matrix(const int row, const int column)
 {
+	if(row < 1 || column < 1)
+		throw "Matrix dimensions must be positive";
 	this->rowSize = row;
 	this->columnSize = column;
 	int i;
@@ -30,6 +32,20 @@ Matrix::~Matrix()
 	delete this->matrix;
 }
 
+// copy constructor for Matrix
+Matrix::Matrix(const Matrix &obj)
+{
+	rowSize = obj.rowSize;
+	columnSize = obj.columnSize;
+	matrix = new int*[rowSize];
+	for(int i = 0; i < rowSize; i++)
+	{
+		matrix[i] = new int[columnSize];
+		for(int j = 0; j < columnSize; j++)
+			matrix[i][j] = obj.matrix[i][j];
+	}
+}
+
 // accessor function for row size
 int Matrix::getRow() const
 {
@@ -46,6 +62,20 @@ int Matrix::getColumn() const
 int** Matrix::getMatrix() const
 {
 	return this->matrix;
+}
+
+// overload for the = operator
+Matrix& Matrix::operator=(Matrix other)
+{
+	int row = other.rowSize;
+	int column = other.columnSize;
+	Matrix* assigned = new Matrix(row, column);
+	for(int i = 0; i < row; i++)
+	{
+		for(int j = 0; i < column; j++)
+			assigned->setEntry(i, j, other.matrix[i][j]);
+	}
+	return *assigned;
 }
 
 // overload for the + operator
@@ -96,3 +126,26 @@ void Matrix::setEntry(int rowIndex, int colIndex, int entry)
 		((this->matrix)[rowIndex])[colIndex] = entry;
 	}
 }
+
+// creates the nxn identify function / Kronecker delta
+Matrix Matrix::Delta(/*Matrix matrix, */int size) // Don't declare static here!
+{
+	if(size < 1)
+		throw "Matrix dimensions must be positive.";
+	Matrix* squareMatrix = new Matrix(size, size);
+	for(int i = 0; i < size; i++)
+	{
+		for(int j = 0; j < size; j++)
+			squareMatrix->setEntry(i, j, (i == j) ? 1 : 0);
+	}
+	return *squareMatrix;
+//	matrix = *squareMatrix;
+}
+
+// TODO Implement Epsilon(int[]) or Epsilon(int, int[]), the "index permuter"
+
+// TODO Implement G(int, int, int), the pseudoRiemannian metric matrix
+// (first integer: start positive or negative; the next two define the signature)
+// Will very much resemble Matrix::Delta
+
+// TODO Define polynomials? Polynomial differentiation? Analytic functions?
